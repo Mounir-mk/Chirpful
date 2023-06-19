b17fb6d7d80a3321719b4be0c2b3be6f0d94eaed
Mounir-mk <mounir.mekoui@gmail.com>
2023-06-19 18:13:29 +0200

    🎨 style(Header.tsx): add a drawer component to the header to improve user experience The drawer component is added to the header to improve the user experience by providing a sidebar menu that can be toggled by clicking on the avatar. The drawer component is implemented using an HTML input element with type checkbox and a label element with a for attribute that matches the input element's id. The drawer component contains a list of menu items that can be customized to suit the application's needs.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,17 +4,20 @@ import React, { useEffect, useRef } from "react";
 import logo from "../assets/logo/logo-focus.png";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import { IoCloseSharp } from "react-icons/io5";
 
 function Header() {
   const pathname = usePathname();
   const headerRef = useRef<HTMLElement>(null);
+
+  const checkboxRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     let lastScrollPosition = 0;
     const header = headerRef.current;
 
     const handleScroll = () => {
-      const currentScrollPosition = window.pageYOffset;
+      const currentScrollPosition = window.scrollY;
 
       if (currentScrollPosition > lastScrollPosition) {
         header?.classList.add("hidden");
@@ -35,7 +38,37 @@ function Header() {
     >
       <div className="avatar absolute top-1/2 left-4 -translate-y-1/2">
         <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-          <img src="https://i.pravatar.cc/300" />
+          <button onClick={() => checkboxRef.current?.click()}>
+            <img src="https://i.pravatar.cc/300" />
+          </button>
+        </div>
+      </div>
+      <div className="drawer absolute">
+        <input
+          ref={checkboxRef}
+          id="my-drawer"
+          type="checkbox"
+          className="drawer-toggle"
+        />
+        <div className="drawer-side">
+          <label htmlFor="my-drawer" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+            {/* Sidebar content here */}
+            <li className="flex flex-row justify-between items-center">
+              <p>Information sur le compte</p>
+              <button
+                type="button"
+                onClick={() => {
+                  checkboxRef.current?.click();
+                }}
+              >
+                <IoCloseSharp className="w-6 h-6" />
+              </button>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
         </div>
       </div>
       {pathname === "/" && (
